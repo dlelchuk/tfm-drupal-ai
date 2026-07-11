cf05838a64405331684fda6372edc2512dedd3d3
Daniel <dlelchuk@gmail.com>
2026-07-11 03:57:40 +0200

    Fallback en caso de error 503 - Limpieza

--- tools/04-update-footer.js
+++
@@ -1,5 +0,0 @@
-const path = require("path");
-
-const SCRIPT_NAME = path.basename(__filename, ".js");
-
-console.log(SCRIPT_NAME);
--- web2/equi-t-academy.eu/server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ========================================
-echo Servidor de pruebas - Sitio web
-echo Puerto: 8000
-echo Pulse Ctrl+C para detenerlo.
-echo ========================================
-echo.
-
-python -m http.server 8000
