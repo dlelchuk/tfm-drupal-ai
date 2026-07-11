ebf68d72f79421cbf870d7230b65aa440c7da9db
Daniel <dlelchuk@gmail.com>
2026-07-11 03:54:38 +0200

    Fallback en caso de error 503

--- backend/renombrarContenedores.bat
+++
@@ -1,4 +0,0 @@
-docker rename tfm-drupal-ai-python-api-1 tfm-drupal-ai-python-api
-docker rename tfm-drupal-ai-db-1 tfm-drupal-ai-db
-docker rename tfm-drupal-ai-drupal-1 tfm-drupal-ai-drupal
-docker rename tfm-drupal-ai-backend-1 tfm-drupal-ai-backend
